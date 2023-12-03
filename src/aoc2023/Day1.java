e73a1b7716dc0f0ab83705a093cb44bcdbe65abf
Homam Jabir <homam.jabir@tietoevry.com>
2023-12-03 21:45:24 +0100

    Cleanup

--- src/aoc2023/Day1.java
+++ src/aoc2023/Day1.java
@@ -36,17 +36,6 @@ public class Day1 {
     }
 
     private static void part2() throws IOException {
-        Map<String, Integer> wordToNumber = new HashMap<>();
-        wordToNumber.put("one", 1);
-        wordToNumber.put("two", 2);
-        wordToNumber.put("three", 3);
-        wordToNumber.put("four", 4);
-        wordToNumber.put("five", 5);
-        wordToNumber.put("six", 6);
-        wordToNumber.put("seven", 7);
-        wordToNumber.put("eight", 8);
-        wordToNumber.put("nine", 9);
-
         int acc = 0;
         try (BufferedReader br = new BufferedReader(new FileReader("src/aoc2023/input/Day1.txt"))) {
             String asd;
