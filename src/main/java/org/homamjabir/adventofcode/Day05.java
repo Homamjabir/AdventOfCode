a65b9469174e987417245aa42db09ed52ec0e84a
Homam Jabir <homam.jabir@tietoevry.com>
2024-12-05 10:11:10 +0100

    Added Day 5 part 2 solution

--- src/main/java/org/homamjabir/adventofcode/Day05.java
+++ src/main/java/org/homamjabir/adventofcode/Day05.java
@@ -36,6 +36,30 @@ public class Day05 {
     }
 
     public static void part2(List<String> input) {
+        Set<String> set = new HashSet<>();
+        int acc = 0;
+        for (String s : input) {
+            if (s.contains("|"))
+                set.add(s);
+            if (s.contains(",")) {
+                boolean flag = false;
+                String[] nums = s.split(",");
+                for (int j = 0; j < nums.length; j++) {
+                    for (int k = j + 1; k < nums.length; k++) {
+                        if (set.contains(nums[k] + "|" + nums[j])) {
+                            String temp = nums[j];
+                            nums[j] = nums[k];
+                            nums[k] = temp;
+                            flag = true;
+                        }
+                    }
+                }
+                if (flag) {
+                    acc += Integer.parseInt(nums[nums.length / 2]);
+                }
+            }
+        }
+        System.out.println("Part 2: " + acc);
     }
 
     public static void main(String[] args) throws IOException {
