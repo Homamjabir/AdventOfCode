54ca5503932f24ea64771f7f9605110bab81920f
Homam Jabir <homam.jabir@tietoevry.com>
2024-12-07 16:16:41 +0100

    Added day 7 part 2

--- src/main/java/org/homamjabir/adventofcode/aoc2024/Day07.java
+++ src/main/java/org/homamjabir/adventofcode/aoc2024/Day07.java
@@ -46,7 +46,51 @@ public class Day07 {
     }
 
     private static void part2(List<String> input) {
+        long acc = 0;
+        for(String line : input) {
+            String[] strings = line.split(": ");
+            long org = Long.parseLong(strings[0]);
+            List<Long> nums = Arrays.stream(strings[1].split(" "))
+                    .map(Long::parseLong)
+                    .toList();
 
+            if (lineValid2(org, nums)) {
+                acc += org;
+            }
+        }
+        System.out.println("Part 1: " + acc);
+    }
+
+    private static boolean lineValid2(long org, List<Long> nums) {
+        return calc2(nums, 0, nums.get(0), org);
+    }
+
+    private static boolean calc2(List<Long> nums, int index, long currentValue, long orgValue) {
+        if (index == nums.size() - 1) {
+            return orgValue == currentValue;
+        }
+
+        long nextValue = nums.get(index + 1);
+
+        if(calc2(nums, index + 1, currentValue * nextValue, orgValue)) {
+            return true;
+        }
+
+        if(calc2(nums, index + 1, currentValue + nextValue, orgValue)) {
+            return true;
+        }
+
+        if(calc2(nums, index + 1, concatNumbers(currentValue, nextValue), orgValue)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static long concatNumbers(long num1, long num2) {
+        String concatenatedString = String.valueOf(num1) + String.valueOf(num2);
+
+        return Long.parseLong(concatenatedString);
     }
 
     public static void main(String[] args) throws IOException {
