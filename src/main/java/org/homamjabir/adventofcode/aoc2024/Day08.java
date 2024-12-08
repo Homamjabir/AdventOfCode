919b1b60e72ce9cd1051d76e91d363cdfeae5b78
Homam Jabir <homam.jabir@tietoevry.com>
2024-12-08 13:41:34 +0100

    Refactoring

--- src/main/java/org/homamjabir/adventofcode/aoc2024/Day08.java
+++ src/main/java/org/homamjabir/adventofcode/aoc2024/Day08.java
@@ -10,14 +10,11 @@ public class Day08 {
     private static void part1(List<String> input) {
         Map<Character, List<String>> map = new HashMap<>();
 
-        for(int i = 0; i < input.size(); i++) {
-            for(int j = 0; j < input.get(i).length(); j++) {
-                if(input.get(i).charAt(j) != '.') {
-                    if(map.get(input.get(i).charAt(j)) == null)
-                        map.put(input.get(i).charAt(j), new ArrayList<>(List.of(i + "," + j)));
-                    else {
-                        map.get(input.get(i).charAt(j)).add(i + "," + j);
-                    }
+        for (int i = 0; i < input.size(); i++) {
+            for (int j = 0; j < input.get(i).length(); j++) {
+                char ch = input.get(i).charAt(j);
+                if (ch != '.') {
+                    map.computeIfAbsent(ch, k -> new ArrayList<>()).add(i + "," + j);
                 }
             }
         }
@@ -27,8 +24,8 @@ public class Day08 {
         for(List<String> list : map.values()) {
             for(int i = 0; i < list.size(); i++) {
                 for(int j = i + 1; j < list.size(); j++) {
-                    int[] cord1 = Arrays.stream(list.get(i).split(",")).mapToInt(Integer::parseInt).toArray();
-                    int[] cord2 = Arrays.stream(list.get(j).split(",")).mapToInt(Integer::parseInt).toArray();
+                    int[] cord1 = extractCords(list.get(i));
+                    int[] cord2 = extractCords(list.get(j));
 
                     int x = cord1[0] - cord2[0];
                     int y = cord1[1] - cord2[1];
@@ -45,6 +42,10 @@ public class Day08 {
         System.out.println("Part 1: " + uniqueCords.size());
     }
 
+    private static int[] extractCords(String cord) {
+        return Arrays.stream(cord.split(",")).mapToInt(Integer::parseInt).toArray();
+    }
+
     private static boolean isValidCord(int c1, int c2, int xLen, int yLen) {
         return (0 <= c1 && c1 < xLen
                 && 0 <= c2 && c2 < yLen);
@@ -53,14 +54,11 @@ public class Day08 {
     private static void part2(List<String> input) {
         Map<Character, List<String>> map = new HashMap<>();
 
-        for(int i = 0; i < input.size(); i++) {
-            for(int j = 0; j < input.get(i).length(); j++) {
-                if(input.get(i).charAt(j) != '.') {
-                    if(map.get(input.get(i).charAt(j)) == null)
-                        map.put(input.get(i).charAt(j), new ArrayList<>(List.of(i + "," + j)));
-                    else {
-                        map.get(input.get(i).charAt(j)).add(i + "," + j);
-                    }
+        for (int i = 0; i < input.size(); i++) {
+            for (int j = 0; j < input.get(i).length(); j++) {
+                char ch = input.get(i).charAt(j);
+                if (ch != '.') {
+                    map.computeIfAbsent(ch, k -> new ArrayList<>()).add(i + "," + j);
                 }
             }
         }
@@ -79,20 +77,14 @@ public class Day08 {
                     uniqueCords.add(cord1[0] + "," + cord1[1]);
                     uniqueCords.add(cord2[0] + "," + cord2[1]);
 
-                    while (true) {
-                        if(isValidCord(cord1[0] + x, cord1[1] + y, input.size(), input.get(0). length()))
-                            uniqueCords.add((cord1[0] + x) + "," + (cord1[1] + y));
-                        else
-                            break;
+                    while (isValidCord(cord1[0] + x, cord1[1] + y, input.size(), input.get(0).length())) {
+                        uniqueCords.add((cord1[0] + x) + "," + (cord1[1] + y));
                         cord1[0] = cord1[0] + x;
                         cord1[1] = cord1[1] + y;
                     }
 
-                    while (true) {
-                        if(isValidCord(cord2[0] - x, cord2[1] - y, input.size(), input.get(0). length()))
-                            uniqueCords.add((cord2[0] - x) + "," + (cord2[1] - y));
-                        else
-                            break;
+                    while (isValidCord(cord2[0] - x, cord2[1] - y, input.size(), input.get(0).length())) {
+                        uniqueCords.add((cord2[0] - x) + "," + (cord2[1] - y));
                         cord2[0] = cord2[0] - x;
                         cord2[1] = cord2[1] - y;
                     }
