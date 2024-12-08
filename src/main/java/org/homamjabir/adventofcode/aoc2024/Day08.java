6bb990c16c396e9f64a5e2e8957c3739cc21983e
Homam Jabir <homam.jabir@tietoevry.com>
2024-12-08 13:32:45 +0100

    Added day 8 part 2 solution

--- src/main/java/org/homamjabir/adventofcode/aoc2024/Day08.java
+++ src/main/java/org/homamjabir/adventofcode/aoc2024/Day08.java
@@ -51,6 +51,56 @@ public class Day08 {
     }
 
     private static void part2(List<String> input) {
+        Map<Character, List<String>> map = new HashMap<>();
+
+        for(int i = 0; i < input.size(); i++) {
+            for(int j = 0; j < input.get(i).length(); j++) {
+                if(input.get(i).charAt(j) != '.') {
+                    if(map.get(input.get(i).charAt(j)) == null)
+                        map.put(input.get(i).charAt(j), new ArrayList<>(List.of(i + "," + j)));
+                    else {
+                        map.get(input.get(i).charAt(j)).add(i + "," + j);
+                    }
+                }
+            }
+        }
+
+        Set<String> uniqueCords = new HashSet<>();
+
+        for(List<String> list : map.values()) {
+            for(int i = 0; i < list.size(); i++) {
+                for(int j = i + 1; j < list.size(); j++) {
+                    int[] cord1 = Arrays.stream(list.get(i).split(",")).mapToInt(Integer::parseInt).toArray();
+                    int[] cord2 = Arrays.stream(list.get(j).split(",")).mapToInt(Integer::parseInt).toArray();
+
+                    int x = cord1[0] - cord2[0];
+                    int y = cord1[1] - cord2[1];
+
+                    uniqueCords.add(cord1[0] + "," + cord1[1]);
+                    uniqueCords.add(cord2[0] + "," + cord2[1]);
+
+                    while (true) {
+                        if(isValidCord(cord1[0] + x, cord1[1] + y, input.size(), input.get(0). length()))
+                            uniqueCords.add((cord1[0] + x) + "," + (cord1[1] + y));
+                        else
+                            break;
+                        cord1[0] = cord1[0] + x;
+                        cord1[1] = cord1[1] + y;
+                    }
+
+                    while (true) {
+                        if(isValidCord(cord2[0] - x, cord2[1] - y, input.size(), input.get(0). length()))
+                            uniqueCords.add((cord2[0] - x) + "," + (cord2[1] - y));
+                        else
+                            break;
+                        cord2[0] = cord2[0] - x;
+                        cord2[1] = cord2[1] - y;
+                    }
+                }
+            }
+        }
+
+        System.out.println("Part 2: " + uniqueCords.size());
     }
 
     public static void main(String[] args) throws IOException {
