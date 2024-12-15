5fcd79e51316440841537a5a12bd623f8e2f07c5
Homam Jabir <homam.jabir@tietoevry.com>
2024-12-15 21:51:19 +0100

    day 14 part 2

--- src/main/java/org/homamjabir/adventofcode/aoc2024/Day14.java
+++ src/main/java/org/homamjabir/adventofcode/aoc2024/Day14.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class Day14 {
 
-    private class Guard {
+    private static class Guard {
         final int vX;
         final int vY;
 
@@ -77,11 +77,75 @@ public class Day14 {
         System.out.println("Part 1: " + result);
     }
 
-
-
     private static void part2(List<Guard> guards) {
+        int cols = 101;
+        int rows = 103;
+
+        for(int i = 0; i < 1000000; i++) {
+
+            for(Guard guard : guards) {
+
+                int newX = guard.posX + guard.vX;
+                int newY = guard.posY + guard.vY;
+
+                if (newX < 0) {
+                    newX = cols + newX;
+                } else if (newX >= cols) {
+                    newX = (newX - cols);
+                }
+
+                if (newY < 0) {
+                    newY = rows + newY;
+                } else if (newY >= rows) {
+                    newY = (newY - rows);
+                }
+                guard.setPosX(newX);
+                guard.setPosY(newY);
+            }
+            if(christmasTreeFound(guards, rows, cols)) {
+                System.out.println(i);
+                break;
+            }
+        }
 
         System.out.println("Part 2: " + 0);
+    }
+
+    private static boolean christmasTreeFound(List<Guard> guards, int rows, int cols) {
+        int[][] field = new int[rows][cols];
+
+        for(Guard guard: guards) {
+            field[guard.posY][guard.posX]++;
+        }
+
+        for(int i = 0; i < field.length; i++) {
+            int counter = 0;
+            for (int j = 0; j < field[0].length; j++) {
+                if(field[i][j] == 0) {
+                    counter++;
+                }
+            }
+            if(counter == 68) {
+                print(field);
+                return true;
+            }
+        }
+        return false;
+
+
+    }
+
+    private static void print(int[][] field) {
+        for(int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[0].length; j++) {
+                if(field[i][j] == 0)
+                    System.out.print(".");
+                else
+                    System.out.print(field[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
 
@@ -95,7 +159,7 @@ public class Day14 {
             String[] parsedString = s.split(" ");
             String[] pos = parsedString[0].replace("p=", "").split(",");
             String[] vel = parsedString[1].replace("v=", "").split(",");
-            guards.add(day14.new Guard(Integer.parseInt(vel[0]), Integer.parseInt(vel[1]), Integer.parseInt(pos[0]), Integer.parseInt(pos[1])));
+            guards.add(new Guard(Integer.parseInt(vel[0]), Integer.parseInt(vel[1]), Integer.parseInt(pos[0]), Integer.parseInt(pos[1])));
         }
 
         part1(guards);
